f71b208a73b8e6587d3d77303200c7b693800b40
JeffJohnson <jeff.jj.9494@gmail.com>
2019-07-25 12:17:10 -0600

    Work In Progress

--- ill-serve-food/src/App.js
+++ ill-serve-food/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component}from 'react';
+import {getData,createData}from './actions/getData.js';
+import {connect}from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home/Home';
+
+
+class App extends Component{
+  state={
+    data:[]
+  }
+ componentDidMount(){
+   console.log(this.props);
+   this.props.getData()
+ }
+  render(){
+    return(
+      <div className="App">
+        <div className="Nav">
+          <div className="Data">
+          <ul>
+            {this.props.data.map(res=>{
+              console.log(res);
+              return(
+              <div >
+                <li>{res}</li>    
+              </div>
+        );
+      })}
+    </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+const mapStateToProps=state=>{
+  console.log(state)
+  return{
+    data:state.data
+  };
+}
+export default connect(
+  mapStateToProps,
+  {getData,createData}
+)(App);
 
-export default App;
---
+++ ill-serve-food/src/actions/getData.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export const GET_DATA='GET_DATA';
+export const GET_DATA_SUCCESS='GET_DATA_SUCCESS';
+export const UPDATING_DATA='UPDATING_DATA';
+export const UPDATE_DATA_SUCCESS='UPDATE_DATA_SUCCESS';
+export const DELETEING_DATA='DELETING_DATA';
+export const CREATING_DATA='CREATING_DATA';
+export const CREATE_DATA='CREATE_DATA';
+export const ERR='ERR';
+
+const URL='https://kitchen-soup-backend.herokuapp.com'
+//get action type
+export const getData=()=>{
+  // axios getting data
+  const data=axios.get(`${URL}`);
+  return dispatch=>{
+    //dispatching responding and catching
+    dispatch({type:GET_DATA});
+      data.then(response=>{
+        dispatch({type:GET_DATA_SUCCESS,payload:response.data});
+      }).catch(err=>{
+        dispatch({type:ERR,payload:err})
+      })
+  };
+};
+// post action type
+export const createData=data=>{
+  //axios posting data
+  const newData=axios.post(`${URL}`,data)
+  return dispatch=>{
+    //dispatching into payload
+    dispatch({type:CREATING_DATA});
+    newData.then(({data})=>{
+      dispatch({type:CREATE_DATA,payload:data});
+    }).catch(err=>{
+      dispatch({type:ERR,payload:err})
+    });
+  };
+}
