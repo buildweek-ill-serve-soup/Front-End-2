dc1d304f8fc3ddb8485a3ce5da25e4dbff9c076b
JeffJohnson <jeff.jj.9494@gmail.com>
2019-07-27 10:50:36 -0600

    added state to sign up form still just at the base of the project but ill be grabbing data soon

--- ill-serve-food/src/components/SignUp/SignupForm.js
+++ ill-serve-food/src/components/SignUp/SignupForm.js
@@ -1,21 +1,69 @@
 import React from 'react';
 
 class SignupForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username:'',
+            email:'',
+            password:'',
+            passwordConfirmation:'',
+            state:''
+        }
+        this.onChange=this.onChange.bind(this);
+        this.onSubmit=this.onSubmit.bind(this);
+    }
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+    onSubmit(e){
+        e.preventDefault();
+        console.log(this.state);
+    }
     render(){
         return(
-        <div>
+        <form onSubmit={this.onSubmit}>
                 <div>
                     <h1>Join Now!</h1>
                 </div>
                 <div className="form-group">
-                   <input type="text"name='username'className='form-controll'/>
+                   <input 
+                        value={this.state.username}
+                        onChange={this.onChange}
+                        placeholder='Username'
+                        type="text"name='username'
+                        className='form-controll'/>
+                        <input 
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        placeholder='Email'
+                        type="text"name='email'
+                        className='form-controll'/>
+                        <input 
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        placeholder='Password'
+                        type="text"name='password'
+                        className='form-controll'/>
+                        <input 
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChange}
+                        placeholder='Confirm Password'
+                        type="text"name='passwordConfirmation'
+                        className='form-controll'/>
+                        <input 
+                        value={this.state.state}
+                        onChange={this.onChange}
+                        placeholder='State'
+                        type="text"name='state'
+                        className='form-controll'/>
                 </div>
                 <div className='form-group'>
-                        <button>
+                        <button >
                             Sign Up
                         </button>
-                </div>
-        </div>
+                </div >
+        </form>
         )
     }
 }
