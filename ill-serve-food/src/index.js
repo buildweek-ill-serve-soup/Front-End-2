a3683884b99a39e60979672c26cf29d44e275bfa
JeffJohnson <jeff.jj.9494@gmail.com>
2019-08-03 07:33:49 -0600

    i have tried everything that i know or can find to get my redux store working but i cant get it working so the only data i actually have access to is users function freeze was thursday i have been up all night working on this and cant figure out anything to get this to work any better im not sure where it went wrong or why the code i have up isnt working because i should have a redux store and i should have tokens in headers but neither of those seem to be working

--- ill-serve-food/src/index.js
+++ ill-serve-food/src/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {browserHistory} from 'react-router';
+import {syncHistoryWithStore}from'react-router-redux';
 import {Provider}from'react-redux';
 import thunk from 'redux-thunk';
-import {createStore,applyMiddleware}from'redux';
+import {createStore,applyMiddleware,compose}from'redux';
 import jwt from 'jsonwebtoken';
 
 
 
-import  reducers from './reducers';
+import rootReducer from './reducers/index.js';
 import App from './App.js';
 import setAuthorizationToken from './utils/setAuthorizationToken.js';
 import { setCurrentUser } from './actions/authActions.js';
-import authReducer from './reducers/authReducer';
 
+const defaultState={
+  data:{}
+}
 const store=createStore(
-  (authReducer),applyMiddleware(thunk)
+  (rootReducer),defaultState,applyMiddleware(thunk)
   
 );
+
 if(localStorage.jwtToken){
   setAuthorizationToken(localStorage.jwtToken);
   store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)));
 }
 setAuthorizationToken(localStorage.jwtToken);
 
+
 ReactDOM.render(
       <Provider store={store}>
-        <Router>
+        
+        <Router >
             <App/>
         </Router>
       </Provider>,
